57dbed3f9753a336bc8817f799f66f77657214a1
Rowan <rowantwell@gmail.com>
2023-12-05 21:45:52 +0000

    require pngs to display

---
+++ Camera.gd
@@ -0,0 +1,12 @@
+extends XRCamera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$WorldHud.rotation_degrees = Vector3(0,0, -(rotation_degrees.z))
+
---
+++ camerahud/camerahud.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+func _ready():
+	# $NoiseVideo.finished.connect(self._finished)
+	pass
+
+func _finished():
+	# loop
+	# $NoiseVideo.play()
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
